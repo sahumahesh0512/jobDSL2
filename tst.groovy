116ea2947301bd3300065490de0d400638a18fe8
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2023-09-06 10:18:30 +0530

    Create tst.groovy

---
+++ tst.groovy
@@ -0,0 +1,18 @@
+pipelineJob('my-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/sahumahesh0512/jobDSL2.git')
+                    }
+                    branches('*/main') // Configure the branch patterns to build
+                    scriptPath('job1-java') // Specify the location of the Jenkinsfile in your repository
+                    scriptPath('job2-javascript')
+                    scriptPath('job2-javascript')
+                    scriptPath('job3-Ruby')
+                }
+            }
+        }
+    }
+}
